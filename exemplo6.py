0adba74a7019d7c1ecd3e482f3c0edd5848bc6da
Tarcianne S.Rocha <tarctata26@gmail.com>
2024-11-07 10:04:57 -0300

    Add files via upload

---
+++ exemplo6.py
@@ -0,0 +1,4 @@
+L=[3, 'abacate', 9.7, [5,6,3], "Python", (3, 'j')]
+print(L[2])
+print(L[3])
+print(L[3][1])
---
+++ exemplo7.py
@@ -0,0 +1,3 @@
+L=[3, 'abacate', 9.7, 'morango' , "Python", (3, 'j')]
+L[3]='morango'
+print(L)
